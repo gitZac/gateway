045d0763eb8d1f9a767b2eb78c5b80447fbf23f1
Zachary Taylor <zachary.taylor@MacBookPro-134.lan>
2025-11-03 08:05:12 -0500

    wip

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -7,9 +7,10 @@ module.exports = ({ env }) => ({
       database: env("DATABASE_NAME", "strapi"),
       user: env("DATABASE_USERNAME", "strapi"),
       password: env("DATABASE_PASSWORD", "strapi"),
-      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      schema: env("DATABASE_SCHEMA", "public"),
       ssl: {
         ca: env("DATABASE_CA"),
+        // rejectUnauthorized: false,
       },
     },
     debug: false,
